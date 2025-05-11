217a3877756bc9715cf7143c05164e8e6b53ace7
holmanworks <164231453+holmanworks@users.noreply.github.com>
2025-05-11 16:24:24 -0700

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/styles/tailwind.css'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next';
+
 
 export const metadata: Metadata = {
   title: {
@@ -26,7 +28,12 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
         />
       </head>
-      <body className="text-gray-950 antialiased">{children}</body>
+
+      <body 
+        className="text-gray-950 antialiased">
+          {children}
+          <Analytics />
+      </body>
     </html>
   )
 }
